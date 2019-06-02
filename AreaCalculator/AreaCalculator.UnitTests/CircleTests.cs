2beb881085812950ac3a4a5f97742387b6a9a2a8
Alex My <alexmy@outlook.com>
2019-06-02 18:43:03 +0300

    Tests

---
+++ AreaCalculator/AreaCalculator.UnitTests/CircleTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AreaCalculator.Models;
+using Xunit;
+
+namespace AreaCalculator.UnitTests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void GetArea_CalculatesCircleArea()
+        {
+            //Arrange
+            var circle = new Circle(5);
+            
+            //Act
+            var area = circle.GetArea();
+            
+            //Assert
+            Assert.Equal(78.53982, area, 5);
+        }
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Constructor_RadiusLessOrEqualZero_ThrowsException(double radius)
+        {
+            Assert.Throws<ArgumentException>(() => new Circle(radius));
+        }
+    }
+}
---
+++ AreaCalculator/AreaCalculator.UnitTests/TriangleTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AreaCalculator.Models;
+using Xunit;
+
+namespace AreaCalculator.UnitTests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void GetArea_CalculatesTriangleArea()
+        {
+            //Arrange
+            var triangle = new Triangle(3.00001, 4.00001, 5.00001);
+            
+            //Act
+            var area = triangle.GetArea();
+            
+            //Assert
+            Assert.Equal(6.00004, area, 5);
+        }
+        
+        [Theory]
+        [InlineData(0, 1, 1)]
+        [InlineData(1, 0, 1)]
+        [InlineData(1, 1, 0)]
+        
+        [InlineData(-1, 1, 1)]
+        [InlineData(1, -1, 1)]
+        [InlineData(1, 1, -1)]
+        public void Constructor_WrongSideValue_ThrowsException(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+        }
+        
+        [Fact]
+        public void Constructor_TriangleImpossible_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(5, 6, 100));
+        }
+        
+        [Fact]
+        public void IsRightTriangle_TriangleIsRight_True()
+        {
+            //Arrange
+            var triangle = new Triangle(3, 4, 5);
+            
+            //Act
+            var isRightTriangle = triangle.IsRightTriangle();
+            
+            //Assert
+            Assert.True(isRightTriangle);
+        }
+        
+        [Fact]
+        public void IsRightTriangle_TriangleIsNOTRight_False()
+        {
+            //Arrange
+            var triangle = new Triangle(3, 4, 6);
+            
+            //Act
+            var isRightTriangle = triangle.IsRightTriangle();
+            
+            //Assert
+            Assert.False(isRightTriangle);
+        }
+    }
+}
