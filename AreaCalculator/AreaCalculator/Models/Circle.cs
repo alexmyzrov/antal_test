830fd10248d701c3f2806b9b3d9cc4e97c3bdc72
Alex My <alexmy@outlook.com>
2019-06-02 18:34:02 +0300

    Figures classes added

---
+++ AreaCalculator/AreaCalculator/Models/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AreaCalculator.Models
+{
+    public class Circle : IFigure
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AreaCalculator/AreaCalculator/Models/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaCalculator.Models
+{
+    public class Triangle : IFigure
+    {
+        public Triangle(double a, double b, double c)
+        {
+        }
+
+        public double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool IsRightTriangle(double tolerance = double.Epsilon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
